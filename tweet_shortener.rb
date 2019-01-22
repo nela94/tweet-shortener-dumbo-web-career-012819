ba55a62fdab15a013f5597e7f99fc29fea0ad5c6
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-22 18:56:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,9 +2,12 @@
 def dictionary
    dictionary = {"hello" => "hi", "to" => 2, "two" => 2, "too" => 2, "for, four" => 4, "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
 end
-def word_substituter(tweet)
-  new_tweet = tweet.split.map! do |word|
-    dictionary(word)
+
+def word_substituter
+tweet_array = tweet.split(" ")
+tweet_array.map do |word|
+  replacements.each do |key, value|
+    word = value if word.downcase == key
   end
-  new_tweet.join(" ")
-end
+  word
+end.join(" ")
