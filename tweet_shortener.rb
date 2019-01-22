7d29c10a07c82a22b50d91fee60fc86a4bdf597b
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-22 18:40:48 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+
+def dictionary
+   dictionary = {"hello" => "hi", "to" => 2, "two" => 2, "too" => 2, "for, four" => 4, "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
+end
+def word_substituter(tweet)
+  new_tweet = tweet.split.map! do |word|
+    dictionary(word)
+  end
+  new_tweet.join(" ")
+end
