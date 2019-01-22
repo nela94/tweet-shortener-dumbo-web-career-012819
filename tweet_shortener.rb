1a5b80955aaea9a1b391a07c87bdefbf085ad09f
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-22 19:40:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,3 +27,11 @@ def selective_tweet_shortener(tweet)
     word_substituter(tweet)
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+   word_substituter(tweet)[0..140] + '...'
+ else
+   tweet
+ end
+end
