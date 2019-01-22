3f8026554c061e0482a495316d3636227f6d6d77
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-22 18:59:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,7 +3,7 @@ def dictionary
    dictionary = {"hello" => "hi", "to" => 2, "two" => 2, "too" => 2, "for, four" => 4, "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
 end
 
-def word_substituter(tweet_array)
+def word_substituter(tweet)
 tweet_array = tweet.split(" ")
 tweet_array.map do |word|
   replacements.each do |key, value|
